a87c59c1bfbe770a6ce04dd2640d13ab07811b2d
Luke <lukiedukester@outlook.com>
2022-01-12 14:26:12 -0800

    Updated Weapons.js

--- src/components/Weapons.js
+++ src/components/Weapons.js
@@ -1,11 +1,17 @@
 import React from "react"
-
+import { Container } from "react-bootstrap"
+import '../css/Weapons.css';
 
 function AboutMe() {
     return (
-        <div>
-            Portfolio!
-        </div>
+        <React.Fragment>
+            <img src="/images/Background.png" className="background" />
+            <Container fluid>
+                <h1 style={{margin: '0 auto', width: 'fit-content', paddingTop: '10%', fontFamily: 'sans-serif', color: 'white'}}>
+                    Coming Soon!
+                </h1>
+            </Container>
+        </React.Fragment>
     )
 }
 
